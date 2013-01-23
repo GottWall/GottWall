4c2478a43d54b30b9faac5ffef6f041dbf7a5f94
Alex <Alexandr.S.rus@gmail.com>
2013-01-23 15:33:14 +0400

    Version update

--- gottwall/__init__.py
+++ gottwall/__init__.py
@@ -14,8 +14,8 @@ Simple statistics aggregator
 __all__ = 'get_version',
 __author__ = "GottWal team"
 __license__ = "BSD, see LICENSE for more details"
-__version_info__ = (0, 2, 2)
-__build__ = 0x000032
+__version_info__ = (0, 2, 3)
+__build__ = 0x000033
 __version__ = ".".join(map(str, __version_info__))
 __maintainer__ = "Alexandr Lispython"
 
