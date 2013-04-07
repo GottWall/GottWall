2278154bf281c0d1c267d0dabe292affbca08eb1
Alex <Alexandr.S.rus@gmail.com>
2013-04-07 16:16:00 +0400

    Version update

--- gottwall/__init__.py
+++ gottwall/__init__.py
@@ -8,6 +8,7 @@ GottWall is a scalable realtime metrics collecting and aggregation platform and 
 
 :copyright: (c) 2012 - 2013 by GottWall team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
+:github: http://github.com/GottWall/GottWall
 """
 
 __all__ = 'get_version',
@@ -25,7 +26,7 @@ if __version__ == 'unknown':
     __version_info__ = (0, 0, 0)
 else:
     __version_info__ = __version__.split('.')
-__build__ = 0x000034
+__build__ = 0x000036
 
 GOTTWALL_HOME = "http://demo.gottwall.com"
 GOTTWALL_DESCRIPTION = "GottWall is a scalable realtime metrics aggrigation platform."
