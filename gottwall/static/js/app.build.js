61f2b4d731f395a9562d26fd5e4dc07a9bf4f85d
Alex <Alexandr.S.rus@gmail.com>
2013-05-11 22:05:03 +0400

    Compiled static for v0.4.1

--- gottwall/static/js/app.build.js
+++ gottwall/static/js/app.build.js
@@ -11050,18 +11050,34 @@ define('js/widgets/chart', ["jquery", "underscore", "swig", "js/widgets/base", "
       });
 
 
-      var x_axis = new Rickshaw.Graph.Axis.X({
-	height: 70,
-	graph: graph,
-	orientation: 'top',
-	ticks: self.get_ticks(graph),
-	//pixelsPerTick: 100,
-	tickFormat: self.tick_formatter,
-	//format: Rickshaw.Fixtures.Number.formatKMBT
-	// format: function(y){
-	//   return y;
-	// }
-      });
+      if (self.gottwall.current_period == 'year'){
+	var x_axis = new Rickshaw.Graph.Axis.X({
+	  height: 70,
+	  graph: graph,
+	  orientation: 'top',
+	  //ticks: self.get_ticks(graph),
+	  //pixelsPerTick: 100,
+	  tickFormat: self.tick_formatter,
+	  //format: Rickshaw.Fixtures.Number.formatKMBT
+	  // format: function(y){
+	  //   return y;
+	  // }
+	});
+      }
+      else {
+	var x_axis = new Rickshaw.Graph.Axis.Time({
+	  height: 70,
+	  graph: graph,
+	  orientation: 'top',
+	  //ticks: self.get_ticks(graph),
+	  //pixelsPerTick: 100,
+	  tickFormat: self.tick_formatter,
+	  //format: Rickshaw.Fixtures.Number.formatKMBT
+	  // format: function(y){
+	  //   return y;
+	  // }
+	});
+      }
 
       var y_axis = new Rickshaw.Graph.Axis.Y({
 	graph: graph,
